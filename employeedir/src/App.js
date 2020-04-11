389e17c5b03671904ad0f37461fa535ce9e07624
Catalina Williams <catalina.rose1361@gmail.com>
2020-04-11 17:40:30 -0600

    App.js skeleton

---
+++ employeedir/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Employee from "./components/Employee";
+
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  // state = {
+  //   friends
+  // };
+
+  
+  // Map over this.state.friends and render a Employee component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <Employee
+            
+            // id={friend.id}
+            // key={friend.id}
+            // name={friend.name}
+            // image={friend.image}
+            // occupation={friend.occupation}
+            // location={friend.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
---
+++ employeedir/src/utils/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+const BASEURL = "https://alanaktion-faker-v1.p.rapidapi.com/lorem/400";
+const APIKEY = "2f1d905d25msh04ea041d8990a07p1e5369jsn51de4c3d2b99";
+
+export default {
+ axios.get(BASEURL + APIKEY);
+  
+};
+
+
+// var settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://alanaktion-faker-v1.p.rapidapi.com/lorem/400",
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "alanaktion-faker-v1.p.rapidapi.com",
+// 		"x-rapidapi-key": "2f1d905d25msh04ea041d8990a07p1e5369jsn51de4c3d2b99"
+// 	}
+// }
+
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
